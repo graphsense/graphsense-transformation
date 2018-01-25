477fb111c79de4ad1e6ba15c0ab160901f0b5f9f
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2018-01-25 17:13:47 +0100

    Rename Scala class in spark-submit script

--- submit.sh
+++ submit.sh
@@ -79,7 +79,7 @@ echo -en "Starting on $CASSANDRA_HOST with master $SPARK_MASTER" \
 
 
 $SPARK_HOME/bin/spark-submit \
-  --class "at.ac.ait.SimpleApp" \
+  --class "at.ac.ait.TransformationJob" \
   --master $SPARK_MASTER \
   --conf spark.executor.memory=$MEMORY \
   --conf spark.cassandra.connection.host=$CASSANDRA_HOST \
