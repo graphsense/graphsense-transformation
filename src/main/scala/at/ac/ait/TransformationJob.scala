095860d4a031bc6dc1c072bc3a5b620ae6e8159e
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2018-01-29 17:47:25 +0100

    Rename tags table in transformed keyspace

--- src/main/scala/at/ac/ait/TransformationJob.scala
+++ src/main/scala/at/ac/ait/TransformationJob.scala
@@ -123,7 +123,7 @@ object TransformationJob {
     save(
       transformation.clusterRelations.sort(F.srcCluster, F.dstCluster),
       "cluster_outgoing_relations")
-    save(transformation.filteredTags, "tag")
+    save(transformation.filteredTags, "address_tags")
     spark.stop()
   }
 }
