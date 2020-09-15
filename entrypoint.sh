090d4bdc329eebd73fdecfec674074de8ebfbda3
Daniel <daniels@cointel.eu>
2020-09-15 20:11:21 +0200

    Add docker-compose support.
    The transformation pipeline can now be run using docker-compose up -d.
    Installing java, sbt, spark, spark master startup, job compilation and execution are all managed by docker.

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd /root
+/usr/local/spark/sbin/start-master.sh
+#./scripts/ingest_test_data.sh
+
+#./scripts/create_target_schema.sh
+#sbt test
+sbt package
+./submit.sh
+
+tail -f /dev/null
