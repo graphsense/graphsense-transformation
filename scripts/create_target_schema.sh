53ff54ce61b9c3e99686a50c799581779848b4ba
Daniels <daniels@cointel.eu>
2020-09-29 20:45:14 +0300

    Also create tagpacks schema

--- scripts/create_target_schema.sh
+++ scripts/create_target_schema.sh
@@ -2,3 +2,6 @@
 
 echo "Creating target keyspace in Cassandra"
 m4 -Dgraphsense=$TARGET_KEYSPACE ./scripts/schema_transformed.cql | cqlsh $CASSANDRA_HOST --cqlversion="3.4.4"
+
+echo "Creating tagpacks keyspace in Cassandra"
+m4 -Dtagkeyspace=$TAG_KEYSPACE ./scripts/schema_tagpacks.cql | cqlsh $CASSANDRA_HOST --cqlversion="3.4.4"
