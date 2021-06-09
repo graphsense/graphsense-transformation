db5635fd053d7bd49f0462d3cf70f617c9082029
dragaschnigm <m.dragaschnig@posteo.net>
2021-06-09 10:07:37 +0200

    Tests for multiple-input clustering

--- src/main/scala/at/ac/ait/Transformator.scala
+++ src/main/scala/at/ac/ait/Transformator.scala
@@ -85,110 +85,109 @@ class Transformator(spark: SparkSession, bucketSize: Int) extends Serializable {
       .drop(F.eur, F.usd)
   }
 
+  def plainAddressCluster(basicTxInputAddresses: DataFrame, removeCoinJoin: Boolean) = {
+    val addressCount = count(F.addressId).over(Window.partitionBy(F.txIndex))
+
+    // filter transactions with multiple input addresses
+    val collectiveInputAddresses = (if (removeCoinJoin) {
+      println(
+        "Clustering without coinjoin inputs"
+      )
+      basicTxInputAddresses.filter(
+        col(F.coinjoin) === false
+      )
+    } else {
+      println(
+        "Clustering with coinjoin inputs"
+      )
+      basicTxInputAddresses
+    })
+      .select(col(F.txIndex), col(F.addressId), addressCount as "count")
+      .filter(col("count") > 1)
+      .select(col(F.txIndex), col(F.addressId))
+
+    // compute number of transactions per address
+    val transactionCount =
+      collectiveInputAddresses.groupBy(F.addressId).count()
+
+    val basicAddressCluster = {
+      // input for clustering algorithm
+      // to optimize performance use only nontrivial addresses,
+      // i.e., addresses which occur in multiple txes
+      // (Spark errors were observed for BTC >= 480000 blocks)
+      val inputGroups = transactionCount
+        .filter(col("count") > 1)
+        .select(F.addressId)
+        .join(collectiveInputAddresses, F.addressId)
+        .groupBy(col(F.txIndex))
+        .agg(collect_set(F.addressId) as "inputs")
+        .select(col("inputs"))
+        .as[InputIdSet]
+        .rdd
+        .toLocalIterator
+
+      spark.sparkContext
+        .parallelize(
+          MultipleInputClustering.getClustersMutable(inputGroups).toSeq
+        )
+        .toDS()
+    }
+
+    val reprAddrId = "reprAddrId"
+
+    // the initial representative must be one address per transaction, which also occurs
+    // in other transactions (tx count >= 2 in collectiveInputAddresses);
+    // choose address with highest tx count
+    val initialRepresentative = {
+
+      val transactionWindow = Window
+        .partitionBy(F.txIndex)
+        .orderBy(col("count").desc, col(F.addressId).asc)
+      val rowNumber = row_number().over(transactionWindow)
+
+      // for each tx, set address which is in most other transactions
+      val addressMax = collectiveInputAddresses
+        .join(transactionCount, F.addressId)
+        .select(
+          col(F.txIndex),
+          col(F.addressId) as reprAddrId,
+          rowNumber as "rank"
+        )
+        .filter(col("rank") === 1)
+        .drop("rank")
+
+      transactionCount
+        .filter(col("count") === 1) // restrict to "trivial" addresses
+        .select(F.addressId)
+        .join(collectiveInputAddresses, F.addressId)
+        .join(addressMax, F.txIndex)
+        .select(F.addressId, reprAddrId)
+    }
+
+    val addressClusterRemainder =
+      initialRepresentative
+        .withColumnRenamed(F.addressId, "id")
+        .join(
+          basicAddressCluster.toDF(reprAddrId, F.cluster),
+          Seq(reprAddrId),
+          "left_outer"
+        )
+        .select(
+          col("id"),
+          coalesce(col(F.cluster), col(reprAddrId)) as F.cluster
+        )
+        .as[Result[Int]]
+
+    basicAddressCluster.union(addressClusterRemainder).toDF()
+  }
+
+
   def addressCluster(
       regularInputs: Dataset[RegularInput],
       addressIds: Dataset[AddressId],
       removeCoinJoin: Boolean
   ) = {
 
-    def plainAddressCluster(
-        basicTxInputAddresses: DataFrame,
-        removeCoinJoin: Boolean
-    ) = {
-
-      val addressCount = count(F.addressId).over(Window.partitionBy(F.txIndex))
-
-      // filter transactions with multiple input addresses
-      val collectiveInputAddresses = (if (removeCoinJoin) {
-                                        println(
-                                          "Clustering without coinjoin inputs"
-                                        )
-                                        basicTxInputAddresses.filter(
-                                          col(F.coinjoin) === false
-                                        )
-                                      } else {
-                                        println(
-                                          "Clustering with coinjoin inputs"
-                                        )
-                                        basicTxInputAddresses
-                                      })
-        .select(col(F.txIndex), col(F.addressId), addressCount as "count")
-        .filter(col("count") > 1)
-        .select(col(F.txIndex), col(F.addressId))
-
-      // compute number of transactions per address
-      val transactionCount =
-        collectiveInputAddresses.groupBy(F.addressId).count()
-
-      val basicAddressCluster = {
-        // input for clustering algorithm
-        // to optimize performance use only nontrivial addresses,
-        // i.e., addresses which occur in multiple txes
-        // (Spark errors were observed for BTC >= 480000 blocks)
-        val inputGroups = transactionCount
-          .filter(col("count") > 1)
-          .select(F.addressId)
-          .join(collectiveInputAddresses, F.addressId)
-          .groupBy(col(F.txIndex))
-          .agg(collect_set(F.addressId) as "inputs")
-          .select(col("inputs"))
-          .as[InputIdSet]
-          .rdd
-          .toLocalIterator
-
-        spark.sparkContext
-          .parallelize(
-            MultipleInputClustering.getClustersMutable(inputGroups).toSeq
-          )
-          .toDS()
-      }
-
-      val reprAddrId = "reprAddrId"
-
-      // the initial representative must be one address per transaction, which also occurs
-      // in other transactions (tx count >= 2 in collectiveInputAddresses);
-      // choose address with highest tx count
-      val initialRepresentative = {
-
-        val transactionWindow = Window
-          .partitionBy(F.txIndex)
-          .orderBy(col("count").desc, col(F.addressId).asc)
-        val rowNumber = row_number().over(transactionWindow)
-
-        val addressMax = collectiveInputAddresses
-          .join(transactionCount, F.addressId)
-          .select(
-            col(F.txIndex),
-            col(F.addressId) as reprAddrId,
-            rowNumber as "rank"
-          )
-          .filter(col("rank") === 1)
-          .drop("rank")
-
-        transactionCount
-          .filter(col("count") === 1) // restrict to "trivial" addresses
-          .select(F.addressId)
-          .join(collectiveInputAddresses, F.addressId)
-          .join(addressMax, F.txIndex)
-          .select(F.addressId, reprAddrId)
-      }
-
-      val addressClusterRemainder =
-        initialRepresentative
-          .withColumnRenamed(F.addressId, "id")
-          .join(
-            basicAddressCluster.toDF(reprAddrId, F.cluster),
-            Seq(reprAddrId),
-            "left_outer"
-          )
-          .select(
-            col("id"),
-            coalesce(col(F.cluster), col(reprAddrId)) as F.cluster
-          )
-          .as[Result[Int]]
-
-      basicAddressCluster.union(addressClusterRemainder).toDF()
-    }
 
     val inputIds = regularInputs
       .join(addressIds, Seq(F.address))
