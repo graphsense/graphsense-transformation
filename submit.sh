33dc6dea46a3024d95a6947df1a7824e7f5faaf2
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2022-01-17 15:36:28 +0100

    Update submit script

--- submit.sh
+++ submit.sh
@@ -20,10 +20,10 @@ if [ -z "$SPARK_HOME" ] ; then
 fi
 
 EXEC=$(basename "$0")
-USAGE="Usage: $EXEC [-h] [-m MEMORY_GB] [-c CASSANDRA_HOST] [-s SPARK_MASTER] [--currency CURRENCY] [--src_keyspace RAW_KEYSPACE] [--tag_keyspace TAG_KEYSPACE] [--tgt_keyspace TGT_KEYSPACE] [--bucket_size BUCKET_SIZE] [--bech32-prefix BECH32_PREFIX]"
+USAGE="Usage: $EXEC [-h] [-m MEMORY_GB] [-c CASSANDRA_HOST] [-s SPARK_MASTER] [--currency CURRENCY] [--raw_keyspace RAW_KEYSPACE] [--tag_keyspace TAG_KEYSPACE] [--tgt_keyspace TGT_KEYSPACE] [--bucket_size BUCKET_SIZE] [--bech32-prefix BECH32_PREFIX] [--checkpoint-dir CHECKPOINT_DIR] [--coinjoin-filtering]"
 
 # parse command line options
-args=$(getopt -o hc:m:s: --long raw_keyspace:,tag_keyspace:,tgt_keyspace:,bucket_size:,currency: -- "$@")
+args=$(getopt -o hc:m:s: --long raw_keyspace:,tag_keyspace:,tgt_keyspace:,bucket_size:,currency:,bech32_prefix:,checkpoint_dir,coinjoin_filtering: -- "$@")
 eval set -- "$args"
 
 while true; do
@@ -64,11 +64,11 @@ while true; do
             BUCKET_SIZE="$2"
             shift 2
         ;;
-        --bech32-prefix)
+        --bech32_prefix)
             BECH32_PREFIX="$2"
             shift 2
         ;;
-        --checkpoint-dir)
+        --checkpoint_dir)
             CHECKPOINT_DIR="$2"
             shift 2
         ;;
